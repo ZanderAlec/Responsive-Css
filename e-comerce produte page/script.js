ed89c58370afabed5d6cc24fa44713d4d946113a
José Alexandre da Silva França <alexandrefranca208@gmail.com>
2023-06-12 16:25:36 -0300

    Carrossel de Imagens

--- e-comerce produte page/script.js
+++ e-comerce produte page/script.js
@@ -1,6 +1,6 @@
 //Menu----------------------------------
 const open_menu_bttm = document.querySelector(".hamburguer-menu");
-const close_menu_bttm = document.querySelector(".close-menu");
+const close_menu_bttm = document.querySelector(".close-bttm--menu");
 const menu = document.querySelector(".menu");
 const fosco_menu_bkg =  document.querySelector(".bkg-dark");
 const body = document.querySelector("body");
@@ -27,19 +27,36 @@ const elem_quant_itens = document.querySelectorAll(".quant-itens");
 const minus_bttm = document.querySelector(".minus-icon");
 const plus_bttm = document.querySelector(".plus-icon");
 
+//Full screen image------------------------------------------------------------------
+const close_full_bttm = document.querySelector(".close-bttm--full");
+const full_display_box = document.querySelector(".full-display-box");
+const prod_img = document.querySelector("#full-bttm");
+
+//Carrossel------------------------------------------------------------
+let currentIndex = 0;
+let currentIndexFull = 0;
+
 
 document.addEventListener('click', (event)=>{
 
    const element = event.target;
 
+   console.log(element);
+
    if(element == open_menu_bttm) openMenu();
    
    if(element == close_menu_bttm) closeMenu();
 
-   if(element == cart_icon){
-        openCloseCart();
-   }
+   if(element == prod_img) openFullDisplay();
 
+   if(element.classList.contains("preview-images")) carrosselBttms(element);
+
+   if(element.classList.contains("slide-button")) carrosselArrows(element);
+   
+   if(element == close_full_bttm) closeFullDisplay();
+
+   if(element == cart_icon) openCloseCart();
+   
    if(element == minus_bttm){
         decreaseAmount();
         displayAmount();
@@ -57,7 +74,6 @@ document.addEventListener('click', (event)=>{
     }
 
     if(element == trash_bttm) cleanCart();
-    
 });
 
 //menu--------------------------------------------
@@ -124,4 +140,132 @@ function addCart(){
 
 function cleanCart(){
     cart_item.style.display = "none";
+}
+
+function openFullDisplay(){
+    fosco_menu_bkg.style.display = "block";
+    full_display_box.style.display = "block";
+}
+
+function closeFullDisplay(){
+    fosco_menu_bkg.style.display = "none";
+    full_display_box.style.display = "none";
+}
+
+function carrosselBttms(element){
+    const element_id = element.id;
+    let indexImg = 0, indexCar = 0; 
+
+    //Full display------------------------------------------------
+    if(element_id == "preview1"){
+
+    }else if(element_id == "preview2"){
+        indexImg = 1;
+
+    }else if(element_id == "preview3"){
+        indexImg = 2;
+        
+    }else if(element_id == "preview4"){
+        indexImg = 3;
+    }
+
+    //Normal display------------------------------------------------
+    else if(element_id == "preview5"){
+        indexCar = 1; 
+
+    }else if(element_id == "preview6"){
+        indexImg = 1;
+        indexCar = 1; 
+
+    }else if(element_id == "preview7"){
+        indexImg = 2;
+        indexCar = 1; 
+
+    }else if(element_id == "preview8"){
+        indexImg = 3;
+        indexCar = 1; 
+    }
+
+    currentIndexFull = indexImg;
+    changeImage(indexImg, indexCar);
+}
+
+function carrosselArrows(element){
+    console.log("entrei");
+    const element_id = element.id;
+    const numImgs = 4;
+    //Pode ser "normal" ou "full"
+    let check = "normal";
+
+    if(element_id == "next"){
+        
+        currentIndex++;
+
+        if(currentIndex == numImgs){
+            currentIndex = 0;
+        }
+    }
+
+    if(element_id == "previous"){
+        currentIndex--;
+
+        if(currentIndex < 0){
+            currentIndex = numImgs-1;
+        }
+    }
+
+    if(element_id == "full-next"){
+        currentIndexFull++;
+
+        if(currentIndexFull == numImgs){
+            currentIndexFull = 0;
+        }
+
+        check = "full";
+    }
+
+    if(element_id == "full-previous"){
+        currentIndexFull--;
+
+        if(currentIndexFull < 0){
+            currentIndexFull = numImgs-1;
+        }
+
+        check = "full";
+    }
+
+    if(check == "normal"){
+        changeImage(currentIndex, 1);
+    }else{
+        changeImage(currentIndexFull, 0);
+    }
+}
+
+function changeImage(indexImg, indexCarrossel){
+    const slide = document.querySelectorAll(".slide-wraper");
+
+    const img1 = '0%';
+    const img2 = '-100%';
+    const img3 = '-200%';
+    const img4 = '-300%';
+
+    if(indexImg == 0){
+        slide[indexCarrossel].style.left = img1;
+        return;
+    }
+
+    if(indexImg == 1){
+        slide[indexCarrossel].style.left = img2;
+        return;
+    }
+
+    if(indexImg == 2){
+        slide[indexCarrossel].style.left = img3;
+        return;
+    }   
+
+    if(indexImg == 3){
+        slide[indexCarrossel].style.left = img4;
+        return;
+    }
 }
